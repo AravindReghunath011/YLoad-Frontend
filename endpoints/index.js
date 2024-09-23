b752612a2ef663b4a07a455b27ccb36e7d73db16
Muhammed Anas NA <anasna6005@gmail.com>
2024-09-23 15:25:38 +0530

    Card added api added for card

--- endpoints/index.js
+++ endpoints/index.js
@@ -1,2 +1,7 @@
-export const SIGNUP_ENDPOINT = "http://localhost:8000/api/user/register"
-export const LOGIN_ENDPOINT = "http://localhost:8000/api/user/login"
+import * as dotenv from 'dotenv'
+
+const BASE_URL = process.env.ENVIRONMENT == "dev" ? "http://localhost:8000" : "https://y-load-backend.vercel.app";
+console.log("Base url ==>" , BASE_URL);
+export const SIGNUP_ENDPOINT = `${BASE_URL}/api/user/register`
+export const LOGIN_ENDPOINT = `${BASE_URL}/api/user/login`
+export const GETUPLOADS_ENDPOINT = `${BASE_URL}/api/user/getuploads`
