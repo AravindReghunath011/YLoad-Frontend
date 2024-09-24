d7dc0daa45684ef5161fd56f0f255c062e8f2499
Aravind <aravindreghunath99@gmail.com>
2024-09-24 12:36:48 +0530

    made landingpage responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import Navbar from '@/components/Navbar'
 
 
 const page = () => {
+  
   return ( 
     <>
 
@@ -15,24 +16,30 @@ const page = () => {
       
       
         <div className='h-screen w-full  text-center flex flex-col justify-center items-center'>
-            <h1 className='text-7xl'
+            <h1 className='text-3xl md:text-5xl xl:text-7xl'
             >Allow Anyone, Anywhere <br /> to Upload <br />
                 <span className='text-green-400'>—With Your Approval.</span>
             </h1>
-            <p className='text-lg mt-8'>
-            Our platform enables seamless video uploads to YouTube from anywhere. <br />
-Editors can collaborate effortlessly, while you maintain control through secure OAuth. <br />
+            <p className='text-xs md:text-lg w-[85%] xl:text-lg mt-8'>
+            Our platform enables seamless video uploads <br className='md:hidden' /> to YouTube  from anywhere. <br className='hidden md:inline-block' />
+Editors can collaborate effortlessly, while  you maintain control through <br className='hidden md:block lg:hidden' /> secure OAuth. <br className='hidden xl:inline-block' />
 No credentials required—just approval for a hassle-free experience!</p>
-            <div className='flex gap-5 mt-10'>
+            <div className='flex gap-5 mt-24 xl:mt-10'>
               <Link href={'/upload'}>
-              <Button  className={'bg-[#121212] text-white p-5 hover:bg-[#121212] hover:border-neutral-700 border w-36 shadow'}>Dashboard</Button>
+              <Button  className={'bg-[#121212] text-white p-2 md:p-5 hover:bg-[#121212] hover:border-neutral-700 border px-4 md:w-36 shadow'}>Dashboard</Button>
             </Link>
-            <Link href={'/uploadnew'}> <Button className={'bg-green-800 border border-green-700 hover:bg-green-800  hover:border-green-600 p-5 w-36 text-white'}>Upload a video</Button></Link>
+            <Link href={'/uploadnew'}> <Button className={'bg-green-800 border border-green-700 hover:bg-green-800  hover:border-green-600 p-2 px-4 md:p-5 md:w-36 text-white'}>Upload a video</Button></Link>
 
             </div>
-            <div className='w-[50%] mt-16 text-xl'>
+            <div className='w-[70%] md:w-50% mt-16 text-xl'>
 
             <Marquee gradient={true} gradientColor='#121212' gradientWidth={300} >
+              <Image src={"/Logo.png"} width={30} height={10} alt='EC'/> &nbsp; EC Upload &nbsp;
+              <Image src={"/Logo.png"} width={30} height={10} alt='EC'/> &nbsp; EC Upload &nbsp;
+              <Image src={"/Logo.png"} width={30} height={10} alt='EC'/> &nbsp; EC Upload &nbsp;
+              <Image src={"/Logo.png"} width={30} height={10} alt='EC'/> &nbsp; EC Upload &nbsp;
+              <Image src={"/Logo.png"} width={30} height={10} alt='EC'/> &nbsp; EC Upload &nbsp;
+              <Image src={"/Logo.png"} width={30} height={10} alt='EC'/> &nbsp; EC Upload &nbsp;
               <Image src={"/Logo.png"} width={30} height={10} alt='EC'/> &nbsp; EC Upload &nbsp;
               <Image src={"/Logo.png"} width={30} height={10} alt='EC'/> &nbsp; EC Upload &nbsp;
               <Image src={"/Logo.png"} width={30} height={10} alt='EC'/> &nbsp; EC Upload &nbsp;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,12 +11,12 @@ const Navbar = () => {
   const { user, setUser } = useContext(UserContext);
   return (
     <div className='w-full h-16   border-b '>
-      <div className='w-[90%] m-auto  h-full flex justify-between '>
+      <div className='w-[90%] m-auto   h-full flex justify-between '>
 
-        <section className=' w-40 h-full text-2xl flex justify-center items-center'>
+        <section className=' w-10  md:w-40  h-full text-2xl flex justify-center items-center'>
           <Link href={'/'}><Image src="/Logo.png" width={35} height={10}  alt='Logo'/></Link>
         </section>
-        <section className=' w-40 h-full flex justify-center items-center gap-5'>
+        <section className=' w-40  h-full flex  justify-end md:justify-center items-center gap-5'>
           <Link href={'https://github.com/AravindReghunath011'} target="_blank" rel="noopener noreferrer">
             <FaGithub size={20} className='text-lightgray hover:text-white' />
           </Link>
